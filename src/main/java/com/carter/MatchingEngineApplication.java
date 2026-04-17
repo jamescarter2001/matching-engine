fc32cf8b66edf64c60f61581543b5c517a7ce86d
James Carter <jamesmcarter7@gmail.com>
2026-04-18 00:50:44 +0100

    Remove message processor from main

--- src/main/java/com/carter/MatchingEngineApplication.java
+++ src/main/java/com/carter/MatchingEngineApplication.java
@@ -1,15 +1,12 @@
 package com.carter;
 
 import com.carter.infra.MatchingEngineServer;
-import com.carter.processor.OrderMessageProcessor;
 
 import java.io.IOException;
 
 public class MatchingEngineApplication {
 
     public static void main(String[] args) {
-        final OrderMessageProcessor messageProcessor = new OrderMessageProcessor();
-
         try (MatchingEngineServer server = new MatchingEngineServer(8080)) {
             server.start();
         } catch (IOException ex) {
